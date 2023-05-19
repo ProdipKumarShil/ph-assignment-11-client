0a785bd7262be889a1b9e4d2a97eff80a3294d3f
ProdipKumarShil <prodipkrishna01@gmail.com>
2023-05-19 22:26:02 +0600

    react tabs are added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../../shared/Banner/Banner';
 import ToySection from '../../shared/ToySection/ToySection';
 import Service from '../../shared/Service/Service';
+import ReactTab from '../../shared/Tab/ReactTab';
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Banner></Banner>
       <ToySection></ToySection>
       {/* <Service></Service> */}
+      <ReactTab></ReactTab>
     </div>
   );
 };
--- src/pages/shared/ToySection/ToySection.jsx
+++ src/pages/shared/ToySection/ToySection.jsx
@@ -1,30 +1,81 @@
 import React, { useEffect, useState } from 'react';
 import ToyCard from './ToyCard';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 
 const ToySection = () => {
 
   const [toys, setToys] = useState([])
+  const [mathToys, setMathToys] = useState([])
+  const [languageToys, setLanguageToys] = useState([])
+  const [scienceToys, setScienceToys] = useState([])
 
   useEffect( () => {
     fetch('toys.json')
       .then(res => res.json())
       .then(data => setToys(data))
   } , [])
+
+  const filterMathToys = () => {
+    const filteredToys = toys.filter(toy => toy.subCategory == 'math')
+    setMathToys(filteredToys)
+    console.log(filteredToys)
+  }
+  const filterLanguageToys = () => {
+    const filteredToys = toys.filter(toy => toy.subCategory == 'language')
+    setLanguageToys(filteredToys)
+    console.log(filteredToys)
+  }
+  const filterScienceToys = () => {
+    const filteredToys = toys.filter(toy => toy.subCategory == 'science')
+    setScienceToys(filteredToys)
+    console.log(filteredToys)
+  }
   
   return (
     <div className='mx-auto max-w-screen-xl'>
       <div className="">
-        <h4 className='text-xl font-semibold text-bg bg-primary py-2 px-3 my-2'>All Category</h4>
-
+        <h4 className='text-xl font-semibold text-bg bg-primary py-2 px-3 my-2'>Shop By Category</h4>
       </div>
 
-      <div className="grid grid-cols-4 gap-4">
-        {
-          toys.slice(0, 8).map(toy => <ToyCard toy={toy}></ToyCard>)
-        }
-      </div>
+      <Tabs>
+        <TabList>
+          <Tab>All Toys</Tab>
+          <Tab onClick={filterMathToys}>Language Toy</Tab>
+          <Tab onClick={filterLanguageToys}>Science Toy</Tab>
+          <Tab onClick={filterScienceToys}>Math Toy</Tab>
+        </TabList>
 
+        <TabPanel>
+          <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+            {
+              toys.slice(0, 20).map(toy => <ToyCard toy={toy}></ToyCard>)
+            }
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+            {
+              languageToys.slice(0, 8).map(toy => <ToyCard toy={toy}></ToyCard>)
+            }
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+            {
+              scienceToys.slice(0, 8).map(toy => <ToyCard toy={toy}></ToyCard>)
+            }
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+            {
+              mathToys.slice(0, 8).map(toy => <ToyCard toy={toy}></ToyCard>)
+            }
+          </div>
+        </TabPanel>
+      </Tabs>
     </div>
   );
 };
