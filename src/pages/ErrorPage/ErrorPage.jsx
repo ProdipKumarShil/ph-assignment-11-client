91f22cf43b16d3c0bfee6d19fc35e934c35b474a
ProdipKumarShil <prodipkrishna01@gmail.com>
2023-05-20 21:32:12 +0600

    added 404 page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Lottie from "lottie-react";
+import anim from '../../assets/lottie/error.json'
+import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
   return (
-    <div>
-      404 page
-      {/* there will be home link */}
+    <div className='flex flex-col'>
+      <Lottie className='p-8 mx-auto' animationData={anim}/>
+      <p className='text-center mb-4 text-3xl'>Page Not Found!!!</p>
+      <Link to={`/`} className="text-bg bg-primary w-1/4 mx-auto text-center rounded-full  font-medium  text-sm px-5 py-3 ">Home</Link>
     </div>
   );
 };
