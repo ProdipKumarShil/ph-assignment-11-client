8cea1504f975a5c4ea0a61d5a45eefbf8f596601
ProdipKumarShil <prodipkrishna01@gmail.com>
2023-05-23 12:21:01 +0600

    final touch

--- src/pages/shared/Banner/Banner.jsx
+++ src/pages/shared/Banner/Banner.jsx
@@ -6,60 +6,52 @@ import img4 from '../../../assets/banner/all5.jpg'
 import img5 from '../../../assets/banner/all6.jpg'
 import sale1 from '../../../assets/seals/seals0.png'
 import Category from '../Category/Category'
+import ReactDOM from 'react-dom';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import Slider from 'react-slick'
+import React from 'react'
 
 const Banner = () => {
+
+  let settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+  
   return (
     <div className='mx-auto max-w-screen-xl grid grid-cols-12 gap-3'>
       {/* categories */}
       <Category></Category>
 
       {/* Carousel  */}
-      <div id="default-carousel" className="mt-2 px-2 relative w-full col-span-12 md:col-span-6" data-carousel="slide">
+      
 
-        <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
+      {/* react carousel */}
 
-          <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src={img1} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+      <div className="md:col-span-6 col-span-12 mt-2">
+        <Slider className='' {...settings}>
+          
+          <div>
+            <img className='h-[390px] rounded-lg w-full' src={img2} alt="" />
           </div>
-
-          <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src={img2} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+          <div>
+            <img className='h-[390px] rounded-lg w-full' src={img3} alt="" />
           </div>
-
-          <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src={img3} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+          <div>
+            <img className='h-[390px] rounded-lg w-full' src={img4} alt="" />
           </div>
-
-          <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src={img4} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+          <div>
+            <img className='h-[390px] rounded-lg w-full' src={img5} alt="" />
           </div>
-
-          <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src={img5} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-          </div>
-        </div>
-
-        <div className="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
-          <button type="button" className="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-          <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-          <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-          <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
-          <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
-        </div>
-
-        <button type="button" className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-          <span className="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg aria-hidden="true" className="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7"></path></svg>
-            <span className="sr-only">Previous</span>
-          </span>
-        </button>
-        <button type="button" className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-          <span className="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg aria-hidden="true" className="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path></svg>
-            <span className="sr-only">Next</span>
-          </span>
-        </button>
+          
+        </Slider>
       </div>
+      
+      {/* react carousel */}
 
       {/* flash sale */}
       <div className="p-4 md:p-0 col-span-12 md:col-span-3 mt-2 relative">
